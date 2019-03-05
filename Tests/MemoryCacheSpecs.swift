3db79b7b539cde0bfb584a6d6b218c5fe65a959e
yysskk <yusuke.0213@gmail.com>
2019-03-05 14:53:20 +0900

    Add tests

--- Tests/MemoryCacheSpecs.swift
+++ Tests/MemoryCacheSpecs.swift
@@ -44,17 +44,66 @@ final class MemoryCacheSpec: QuickSpec {
         }
 
         describe("load") {
-            beforeEach {
-                memoryCache.removeAll()
-                memoryCache.set(dog, for: .dog)
+            context("normal") {
+                beforeEach {
+                    memoryCache.removeAll()
+                    memoryCache.set(dog, for: .dog)
+                }
+
+                it("should be dog") {
+                    do {
+                        let cache = try memoryCache.load(for: .dog)
+                        expect(cache.value.name).to(be(dog.name))
+                    } catch {
+                        fail("Catch \(error.localizedDescription)")
+                    }
+                }
             }
 
-            it("should be dog") {
-                do {
-                    let cache = try memoryCache.load(for: .dog)
-                    expect(cache.value.name).to(be(dog.name))
-                } catch {
-                    fail("Catch \(error.localizedDescription)")
+            context("notFound") {
+                beforeEach {
+                    memoryCache.removeAll()
+                }
+
+                it("should catch MemoryCacheError.expired.notFound error") {
+                    do {
+                        let cache = try memoryCache.load(for: .dog)
+                        fail("Catch \(cache.value)")
+                    } catch {
+                        guard let memoryCacheError = error as? MemoryCacheError else {
+                            fail("it doesn't match \(MemoryCacheError.self) type")
+                            return
+                        }
+                        expect(memoryCacheError == .notFound).to(beTrue())
+                    }
+                }
+            }
+
+            context("expired") {
+                var expiration: Expiration!
+
+                beforeEach {
+                    memoryCache.removeAll()
+                    expiration = .date(Date(timeIntervalSinceNow: -60))
+                    memoryCache.set(dog, for: .dog, expiration: expiration)
+                }
+
+                it("should catch MemoryCacheError.expired error") {
+                    do {
+                        let cache = try memoryCache.load(for: .dog)
+                        fail("Catch \(cache.value)")
+                    } catch {
+                        guard let memoryCacheError = error as? MemoryCacheError else {
+                            fail("it doesn't match \(MemoryCacheError.self) type")
+                            return
+                        }
+                        guard case .expired(let date) = memoryCacheError else {
+                            fail("it isn't .expired")
+                            return
+                        }
+
+                        expect(date.timeIntervalSinceReferenceDate == expiration?.date.timeIntervalSinceReferenceDate).to(beTrue())
+                    }
                 }
             }
         }
@@ -67,25 +116,63 @@ final class MemoryCacheSpec: QuickSpec {
                 memoryCache.remove(for: .dog)
             }
 
-            it("should be nil") {
-                do {
-                    let cache = try memoryCache.load(for: .dog)
-                    fail("Catch \(cache.value)")
-                } catch {
-                    guard let memoryCacheError = error as? MemoryCacheError else {
-                        fail("it not match \(MemoryCacheError.self) type")
-                        return
+            context("normal") {
+                beforeEach {
+                    memoryCache.removeAll()
+                    memoryCache.set(dog, for: .dog)
+                }
+
+                it("should be dog") {
+                    do {
+                        let cache = try memoryCache.load(for: .dog)
+                        expect(cache.value.name).to(be(dog.name))
+                    } catch {
+                        fail("Catch \(error.localizedDescription)")
                     }
-                    expect(memoryCacheError == .notFound).to(beTrue())
+                }
+            }
+        }
+
+        describe("removeIfExpired") {
+            var expiration: Expiration!
+
+            context("normal") {
+                beforeEach {
+                    memoryCache.removeAll()
+                    expiration = .date(Date(timeIntervalSinceNow: 60 * 60))
+                    memoryCache.set(dog, for: .dog, expiration: expiration)
+                    memoryCache.removeIfExpired(for: .dog)
+                }
+
+                it("should be dog") {
+                    do {
+                        let cache = try memoryCache.load(for: .dog)
+                        expect(cache.value.name).to(be(dog.name))
+                    } catch {
+                        fail("Catch \(error.localizedDescription)")
+                    }
                 }
             }
 
-            it("should not be nil") {
-                do {
-                    let cache = try memoryCache.load(for: .cat)
-                    expect(cache.value.name).to(be(cat.name))
-                } catch {
-                    fail("Catch \(error.localizedDescription)")
+            context("If the cache expired") {
+                beforeEach {
+                    memoryCache.removeAll()
+                    expiration = .date(Date(timeIntervalSinceNow: -60))
+                    memoryCache.set(dog, for: .dog, expiration: expiration)
+                    memoryCache.removeIfExpired(for: .dog)
+                }
+
+                it("should catch MemoryCacheError.notFound error") {
+                    do {
+                        let cache = try memoryCache.load(for: .dog)
+                        fail("Catch \(cache.value)")
+                    } catch {
+                        guard let memoryCacheError = error as? MemoryCacheError else {
+                            fail("it doesn't match \(MemoryCacheError.self) type")
+                            return
+                        }
+                        expect(memoryCacheError == .notFound).to(beTrue())
+                    }
                 }
             }
         }
@@ -98,13 +185,13 @@ final class MemoryCacheSpec: QuickSpec {
                 memoryCache.removeAll()
             }
 
-            it("should be nil") {
+            it("should catch MemoryCacheError.notFound error") {
                 do {
                     let cache = try memoryCache.load(for: .dog)
                     fail("Catch \(cache.value)")
                 } catch {
                     guard let memoryCacheError = error as? MemoryCacheError else {
-                        fail("it not match \(MemoryCacheError.self) type")
+                        fail("it doesn't match \(MemoryCacheError.self) type")
                         return
                     }
                     expect(memoryCacheError == .notFound).to(beTrue())
@@ -115,10 +202,121 @@ final class MemoryCacheSpec: QuickSpec {
                     fail("Catch \(cache.value)")
                 } catch {
                     guard let memoryCacheError = error as? MemoryCacheError else {
-                        fail("it not match \(MemoryCacheError.self) type")
+                        fail("it doesn't match \(MemoryCacheError.self) type")
                         return
                     }
                     expect(memoryCacheError == .notFound).to(beTrue())
+                }
+            }
+        }
+
+        describe("Expiration") {
+            var expiration: Expiration!
+
+            context(".never") {
+                beforeEach {
+                    memoryCache.removeAll()
+                    expiration = .never
+                    memoryCache.set(dog, for: .dog, expiration: expiration)
+                }
+
+                it("should be dog") {
+                    do {
+                        let cache = try memoryCache.load(for: .dog)
+                        expect(cache.value.name).to(be(dog.name))
+                    } catch {
+                        fail("Catch \(error.localizedDescription)")
+                    }
+                }
+            }
+
+            context(".seconds") {
+                context("normal") {
+                    beforeEach {
+                        memoryCache.removeAll()
+                        expiration = .seconds(60 * 60)
+                        memoryCache.set(dog, for: .dog, expiration: expiration)
+                    }
+
+                    it("should be dog") {
+                        do {
+                            let cache = try memoryCache.load(for: .dog)
+                            expect(cache.value.name).to(be(dog.name))
+                        } catch {
+                            fail("Catch \(error.localizedDescription)")
+                        }
+                    }
+                }
+
+                context("If the cache expired") {
+                    beforeEach {
+                        memoryCache.removeAll()
+                        expiration = .seconds(-60)
+                        memoryCache.set(dog, for: .dog, expiration: expiration)
+                    }
+
+                    it("should catch MemoryCacheError.expired error") {
+                        do {
+                            let cache = try memoryCache.load(for: .dog)
+                            fail("Catch \(cache.value)")
+                        } catch {
+                            guard let memoryCacheError = error as? MemoryCacheError else {
+                                fail("it doesn't match \(MemoryCacheError.self) type")
+                                return
+                            }
+                            guard case .expired(let date) = memoryCacheError else {
+                                fail("it isn't .expired")
+                                return
+                            }
+
+                            expect(date < Date().addingTimeInterval(-60)).to(beTrue())
+                        }
+                    }
+                }
+            }
+
+            context(".date") {
+                context("normal") {
+                    beforeEach {
+                        memoryCache.removeAll()
+                        expiration = .date(Date(timeIntervalSinceNow: 60 * 60))
+                        memoryCache.set(dog, for: .dog, expiration: expiration)
+                    }
+
+                    it("should be dog") {
+                        do {
+                            let cache = try memoryCache.load(for: .dog)
+                            expect(cache.value.name).to(be(dog.name))
+                        } catch {
+                            fail("Catch \(error.localizedDescription)")
+                        }
+                    }
+                }
+
+                context("If the cache expired") {
+                    beforeEach {
+                        memoryCache.removeAll()
+                        expiration = .date(Date(timeIntervalSinceNow: -60))
+                        memoryCache.set(dog, for: .dog, expiration: expiration)
+                    }
+
+                    it("should catch MemoryCacheError.expired error") {
+                        do {
+                            let cache = try memoryCache.load(for: .dog)
+                            fail("Catch \(cache.value)")
+                        } catch {
+                            guard let memoryCacheError = error as? MemoryCacheError else {
+                                fail("it doesn't match \(MemoryCacheError.self) type")
+                                return
+                            }
+                            guard case .expired(let date) = memoryCacheError else {
+                                fail("it isn't .expired")
+                                return
+                            }
+
+                            expect(date.timeIntervalSinceReferenceDate == expiration?.date.timeIntervalSinceReferenceDate).to(beTrue())
+                        }
+                    }
                 }
             }
         }
